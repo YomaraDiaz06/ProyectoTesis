8b81cb6f7685201f3f2b6c6cf8f4b88448f902cc
Elvis Agila <elvis.agila@epn.edu.ec>
2021-06-27 22:14:48 -0500

    interfaces y rutas

--- tecnifast/src/app/app.module.ts
+++ tecnifast/src/app/app.module.ts
@@ -2,15 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import{AngularFireModule} from '@angular/fire';
+import {environment} from 'src/environments/environment';
+import {AngularFirestore} from '@angular/fire/firestore';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HeaderModule } from './componentes/header/header.module';
+import { InformationComponent } from './pages/information/information.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    InformationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    AngularFireAuthModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    HeaderModule,
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ tecnifast/src/app/componentes/header/header.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FirebaseauthService } from 'src/app/services/firebaseauth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  @Output()
+  toggleSidebar = new EventEmitter<void>();
+
+  public showSearch = false;
+  usuario: string='';
+
+  constructor(private serviceauth:FirebaseauthService) { }
+
+  ngOnInit(): void {
+    try{
+      this.serviceauth.getCurrentUser().then(r=>{
+      
+
+       console.log(r?.email);
+
+     })
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  Onlogout(){
+
+    this.serviceauth.logout();
+  }
+
+}
