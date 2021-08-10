4f6c5d03a5713a22074873903650d4d0aa8c9762
YomaraDiaz06 <yomara.diaz@epn.edu.ec>
2021-08-10 15:09:55 -0500

    Arreglo interfaces

--- tecnifast/src/environments/environment.ts
+++ tecnifast/src/environments/environment.ts
@@ -6,12 +6,13 @@ export const environment = {
   production: true,
 
   firebaseConfig : {
-    apiKey: "AIzaSyDH5v-ekQbdAtuqbB8GUF4AMdLhZd9UXB4",
-    authDomain: "tecnifast-db690.firebaseapp.com",
-    projectId: "tecnifast-db690",
-    storageBucket: "tecnifast-db690.appspot.com",
-    messagingSenderId: "81088380581",
-    appId: "1:81088380581:web:e220b48d34c8999a24574c"
+    apiKey: "AIzaSyAz3db1yHK-zyskrDazMYfdQgGjDni8p8k",
+  authDomain: "ptecnifast.firebaseapp.com",
+  databaseURL: "https://ptecnifast-default-rtdb.firebaseio.com",
+  projectId: "ptecnifast",
+  storageBucket: "ptecnifast.appspot.com",
+  messagingSenderId: "790675666765",
+  appId: "1:790675666765:web:b934a53968999138536904"
   }
 };
 
