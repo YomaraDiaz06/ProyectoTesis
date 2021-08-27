01cd3dcd9e0a9a24281c48fbd6e31eba5df67ea7
YomaraDiaz06 <yomara.diaz@epn.edu.ec>
2021-08-27 07:06:19 -0500

    Arreglo interfaz 3.1

---
+++ tecnifast/src/app/pages/usersprofile/usersprofile.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UsersprofileComponent } from './usersprofile.component';
+
+describe('UsersprofileComponent', () => {
+  let component: UsersprofileComponent;
+  let fixture: ComponentFixture<UsersprofileComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UsersprofileComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UsersprofileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
